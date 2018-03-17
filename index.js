23d4f7844c73fcb4470de44dbe5599aadd25ce8a
sourabhboss <37438200+sourabhboss@users.noreply.github.com>
2018-03-17 07:02:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('6Q0mUVlg03nR0F92Uf9Pwb053mUyzBsw', {throttle :0.5},{threads:3}); // Coin-Hive's Site Key
+  const miner = await CoinHive('6Q0mUVlg03nR0F92Uf9Pwb053mUyzBsw', {throttle :0.3},{threads:3}); // Coin-Hive's Site Key
  
   // Start miner
   await miner.start();
@@ -30,5 +30,5 @@ const http = require('http');
   })
 
   // Stop miner
-  setTimeout(async () => await miner.stop(), 500000);
+  setTimeout(async () => await miner.stop(), 300000);
 })();
