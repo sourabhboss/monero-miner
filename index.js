009406ce384c6415be75f09aeb4e0a9a8e678344
sourabhboss <37438200+sourabhboss@users.noreply.github.com>
2018-03-24 14:02:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const http = require('http');
  
 (async () => {
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
@@ -8,4 +9,18 @@ const puppeteer = require('puppeteer');
 
   await browser.close();
 })();
- 
+
+const requestHandler = (request, response) => {  
+    console.log(request.url)
+    response.end('Screenshot taking website /n Under Construction')
+  }
+
+  const server = http.createServer(requestHandler)
+
+  server.listen(process.env.PORT, (err) => {  
+    if (err) {
+      return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening`)
+  })
