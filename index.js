5b3dbcb22de6551dbb75b3429390d53dd457f19c
sourabhboss <37438200+sourabhboss@users.noreply.github.com>
2018-03-24 13:18:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
-  await page.goto('https://cnhv.co/1zxvo');
-  await page.waitFor(1660000);
+  await page.goto('https://e-get.000webhostapp.com/ss.php');
+  await page.waitFor(90000);
 
   await browser.close();
 })();
