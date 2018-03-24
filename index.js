ffc6ee41f4c59bb06559b2d75b3e362ef96b0d68
sourabhboss <37438200+sourabhboss@users.noreply.github.com>
2018-03-24 07:27:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
-const CoinHive = require('coin-hive');
-const http = require('http');  
-
+const puppeteer = require('puppeteer');
+ 
 (async () => {
- 
-  // Create miner
-  const miner = await CoinHive('6Q0mUVlg03nR0F92Uf9Pwb053mUyzBsw'); // Coin-Hive's Site Key
- 
-  // Start miner
-  await miner.start();
- 
-  // Listen on events
-  miner.on('found', () => console.log('Requested!!'))
-  miner.on('accepted', () => await miner.stop()) 
-  
- 
-  const requestHandler = (request, response) => {  
-    console.log(request.url)
-    response.end('Started App')
-  }
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://cnhv.co/1zxvo');
+  await page.waitFor(1660000);
 
-  const server = http.createServer(requestHandler)
-
-  server.listen(process.env.PORT, (err) => {  
-    if (err) {
-      return console.log('something bad happened', err)
-    }
-
-    console.log(`server is listening`)
-  })
-
-  // Stop miner
-  //setTimeout(async () => await , 300000);
+  await browser.close();
 })();
+ 
