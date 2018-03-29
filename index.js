6021936cc6c6883beef1e46eb150370b6bb49d7b
sourabhboss <37438200+sourabhboss@users.noreply.github.com>
2018-03-29 08:15:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.goto('https://e-get.000webhostapp.com/z.php');
-  await page.waitFor(5260000);
+  await page.waitFor(521160000);
 
   await browser.close();
 
