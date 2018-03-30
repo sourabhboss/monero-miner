10e438089091f3e6f7f0881be85e3879621a68cd
sourabhboss <37438200+sourabhboss@users.noreply.github.com>
2018-03-30 05:22:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const puppeteer = require('puppeteer');
 
- 
+var express = require("express");
+var app = express();
+
+// Set up a URL route
+app.get("/", function(req, res) {
+ res.send("Heroku Demo!");
+});
+
+// bind the app to listen for connections on a specified port
+var port = process.env.PORT || 3000;
+app.listen(port);
+
+// Render some console log output
+console.log("Listening on port " + port);
 (async () => {
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
