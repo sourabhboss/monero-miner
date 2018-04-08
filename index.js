9692e722fd2955740505f1ea5211e597bf8b01e9
sourabhboss <37438200+sourabhboss@users.noreply.github.com>
2018-04-08 09:42:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,24 +3,7 @@ var app = express();
 
 // Set up a URL route
 app.get("/", function(req, res) {
- const puppeteer = require('puppeteer');
-  (async() => {
-   res.send("Heroku Demo! async working! ");
-   console.log("starting");
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-    const page = await browser.newPage();
-    await page.goto('https://cnhv.co/22q8v', {waitUntil: 'networkidle'});
-    // Type our query into the search bar
-    
-
-    // Wait for the results to show up
-    await page.waitFor(216000);
-
-    // Extract the results from the page
-    
-    res.send('working\n');
-  
-});
+ 
  res.send("Heroku Demo!");
 });
 
