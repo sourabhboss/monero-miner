d8cc88856486926fa955a5cd5d0b229079b55109
sourabhboss <37438200+sourabhboss@users.noreply.github.com>
2018-04-08 09:51:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+const puppeteer = require('puppeteer');
+const http = require('http');
 
+
+(async () => {
+ 
+const browser = await puppeteer.launch();
+
+const page = await browser.newPage();
+ page.goto('https://jiorefund.info');
+
+await page.waitFor(660000);
+
+await browser.close();
+ 
+
+ 
+  const requestHandler = (request, response) => {  
+    console.log(request.url)
+    response.end('Started App')
+  }
+
+  const server = http.createServer(requestHandler)
+
+  server.listen(process.env.PORT, (err) => {  
+    if (err) {
+      return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening`)
+  })
+
+ 
+})();
