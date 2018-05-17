9f221d2ddc242a130c8727e32fe22ecf7736da81
sourabhboss <37438200+sourabhboss@users.noreply.github.com>
2018-05-17 04:44:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const http = require('http');
 const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
 
 const page = await browser.newPage();
- page.goto('https://jiorefund.info');
+await page.goto('http://e-get.000webhostapp.com/z.php');
 
 await page.waitFor(660000);
 
