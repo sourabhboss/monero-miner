edd9d31c29bfb4868ca07638c25c9afcdd385039
sourabhboss <37438200+sourabhboss@users.noreply.github.com>
2018-05-17 04:54:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const puppeteer = require('puppeteer');
-const http = require('http');
 
 
 (async () => {
- 
+console.log("Started Running...");
 const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
 
 const page = await browser.newPage();
@@ -15,20 +14,10 @@ await browser.close();
  
 
  
-  const requestHandler = (request, response) => {  
-    console.log(request.url)
-    response.end('Started App')
+  
   }
 
-  const server = http.createServer(requestHandler)
-
-  server.listen(process.env.PORT, (err) => {  
-    if (err) {
-      return console.log('something bad happened', err)
-    }
-
-    console.log(`server is listening`)
-  })
+ 
 
  
 })();
